4976eab64773b91b2730c1c9b5290e9b62919770
Jen-Shen <anischenko2016@yandex.ru>
2023-09-26 16:21:55 +0300

    lab2

--- main.py
+++ main.py
@@ -18,15 +18,18 @@ start_point = (0, 0)
 end_point = (0, 0)
 file = ""
 
+
 def select_algorithm(algorithm):
     global selected_algorithm
     selected_algorithm = algorithm
+
 
 def load_data(filename):
     with open(filename, 'r') as file_obj:
         data = file_obj.readlines()
     file_obj.close()
     return data
+
 
 def toggle_debug_mode():
     data = load_data(file_name)
@@ -43,6 +46,7 @@ def toggle_debug_mode():
 
     root.mainloop()
 
+
 def dda_algorithm(canvas):
     global file_name
     file_name = "dda_algorithm_steps.txt"
@@ -55,12 +59,12 @@ def dda_algorithm(canvas):
 
     if length == 0:
         length = 1
-    #print(x_r, y_r, length)
+    # print(x_r, y_r, length)
     dx = x_r / length
     dy = y_r / length
 
-    x = x0 + 0.5*np.sign(dx)
-    y = y0 + 0.5*np.sign(dy)
+    x = x0 + 0.5 * np.sign(dx)
+    y = y0 + 0.5 * np.sign(dy)
 
     for i in range(length):
         x = x + dx
@@ -69,6 +73,7 @@ def dda_algorithm(canvas):
         with open("dda_algorithm_steps.txt", "a") as file:
             file.write(f"Step {i}, x:{x}, y:{y}, Plot({x},{y})\n")
         file.close()
+
 
 def bresenham_algorithm(canvas):
     global file_name
@@ -108,6 +113,7 @@ def bresenham_algorithm(canvas):
             file.write(f"Step {i}, e:{error}, x:{x}, y:{y}, Plot({x},{y})\n")
         file.close()
 
+
 def wu_algorithm(canvas):
     global file_name
     file_name = "wu_algorithm_steps.txt"
@@ -141,14 +147,16 @@ def wu_algorithm(canvas):
             a = abs(e)
             fill_color = "#%02x%02x%02x" % (int(a * 255), int(a * 255), int(a * 255))
             if e <= 0:
-                canvas.create_rectangle(x - changeX, y - changeY, x - changeX + 1, y - changeY + 1, fill=fill_color, outline=fill_color)
+                canvas.create_rectangle(x - changeX, y - changeY, x - changeX + 1, y - changeY + 1, fill=fill_color,
+                                        outline=fill_color)
             else:
-                canvas.create_rectangle(x + changeX, y + changeY, x + changeX + 1, y + changeY + 1, fill=fill_color, outline=fill_color)
-            #print(f"Step {i}: e:{e}, x:{x}; y:{y}, a:{a} Plot({x},{y})")
+                canvas.create_rectangle(x + changeX, y + changeY, x + changeX + 1, y + changeY + 1, fill=fill_color,
+                                        outline=fill_color)
+            # print(f"Step {i}: e:{e}, x:{x}; y:{y}, a:{a} Plot({x},{y})")
             i += 1
     else:
         e = dx / dy - 0.5
-        #print(f"Step 0: x:{x}; y:{y}, e':{e} Plot({x},{y})")
+        # print(f"Step 0: x:{x}; y:{y}, e':{e} Plot({x},{y})")
         while i <= dy:
             if e >= 0:
                 x += changeX
@@ -159,14 +167,17 @@ def wu_algorithm(canvas):
             fill_color = "#%02x%02x%02x" % (int(a * 255), int(a * 255), int(a * 255))
             canvas.create_rectangle(x, y, x + 1, y + 1, fill=fill_color, outline=fill_color)
             if e <= 0:
-                canvas.create_rectangle(x - changeX, y - changeY, x - changeX + 1, y - changeY + 1, fill=fill_color, outline=fill_color)
+                canvas.create_rectangle(x - changeX, y - changeY, x - changeX + 1, y - changeY + 1, fill=fill_color,
+                                        outline=fill_color)
             else:
-                canvas.create_rectangle(x + changeX, y + changeY, x + changeX + 1, y + changeY + 1, fill=fill_color, outline=fill_color)
-            #print(f"Step {i}: e:{e}, x:{x}; y:{y}, a:{a} Plot({x},{y})")
+                canvas.create_rectangle(x + changeX, y + changeY, x + changeX + 1, y + changeY + 1, fill=fill_color,
+                                        outline=fill_color)
+            # print(f"Step {i}: e:{e}, x:{x}; y:{y}, a:{a} Plot({x},{y})")
             with open("wu_algorithm_steps.txt", "a") as file:
                 file.write(f"Step {i}: e:{e}, x:{x}; y:{y}, a:{a} Plot({x},{y})\n")
             i += 1
         file.close()
+
 
 def find_radius(x1, y1, x2, y2):
     r1 = x2 - x1
@@ -175,19 +186,26 @@ def find_radius(x1, y1, x2, y2):
     l_min = min(abs(r1), abs(r2))
     return math.sqrt(pow(l_max, 2) + pow(l_min, 2))
 
+
+def find_a_b(x1, y1, x2, y2):
+    a = abs(x2 - x1)
+    b = abs(y2 - y1)
+    return a, b
+
+
 def draw_circle(x, origin_x, y, origin_y):
+    radius = 1
+    canvas.create_oval(x - radius + origin_x, y - radius + origin_y, x + radius + 1 + origin_x,
+                       y + radius + 1 + origin_y, fill="black")
+    canvas.create_oval(x - radius + origin_x, -y - radius + origin_y, x + radius + 1 + origin_x,
+                       -y + radius + 1 + origin_y, fill="black")
+    canvas.create_oval(-x - radius + origin_x, y - radius + origin_y, -x + radius + 1 + origin_x,
+                       y + radius + 1 + origin_y, fill="black")
+    canvas.create_oval(-x - radius + origin_x, -y - radius + origin_y, -x + radius + 1 + origin_x,
+                       -y + radius + 1 + origin_y, fill="black")
 
-        radius = 1
-        canvas.create_oval(x - radius + origin_x, y - radius + origin_y, x + radius + 1 + origin_x,
-                           y + radius + 1 + origin_y, fill="black")
-        canvas.create_oval(x - radius + origin_x, -y - radius + origin_y, x + radius + 1 + origin_x,
-                           -y + radius + 1 + origin_y, fill="black")
-        canvas.create_oval(-x - radius + origin_x, y - radius + origin_y, -x + radius + 1 + origin_x,
-                           y + radius + 1 + origin_y, fill="black")
-        canvas.create_oval(-x - radius + origin_x, -y - radius + origin_y, -x + radius + 1 + origin_x,
-                           -y + radius + 1 + origin_y, fill="black")
 
-def circle(canvas):
+def circle_algorithm(canvas):
     global file_name
     file_name = "circle_steps.txt"
 
@@ -227,7 +245,54 @@ def circle(canvas):
             delta += 2 * x - 2 * y + 2
             draw_circle(x, origin_x, y, origin_y)
             i = i + 1
-            file.write(f"Step {i}, e:{delta},d:{d},,d*{dz} x:{x+origin_x}, y:{y+origin_y}, Plot({x+origin_x},{y+origin_y})\n")
+            file.write(
+                f"Step {i}, e:{delta},d:{d},,d*{dz} x:{x + origin_x}, y:{y + origin_y}, Plot({x + origin_x},{y + origin_y})\n")
+    file.close()
+
+
+def ellipse_algorithm(canvas):
+    global file_name
+    file_name = "ellipse_steps.txt"
+
+    canvas_width = canvas.winfo_width()
+    canvas_height = canvas.winfo_height()
+    origin_x = canvas_width // 2
+    origin_y = canvas_height // 2
+
+    x1, y1 = start_point
+    x2, y2 = end_point
+
+    a, b = find_a_b(x1, y1, x2, y2)
+
+    aPow2 = int(math.pow(a, 2))
+    bPow2 = int(math.pow(b, 2))
+    x = 0
+    y = b
+    limit = y - b
+    delta = aPow2 + bPow2 - 2 * aPow2 * b
+    # drawEllipsePoints(context, center, 0, 0, 0, 0, '0', x, y, delta)
+    i = 0
+    with open("ellipse_steps.txt", "a") as file:
+        while y > limit:
+            dz = 2 * delta - 2 * x * bPow2 - 1
+            if delta > 0 and dz > 0:
+                y -= 1
+                delta += aPow2 - 2 * y * aPow2
+                draw_circle(x, origin_x, y, origin_y)
+                continue
+            d = 2 * delta + 2 * y * aPow2 - 1
+            if delta < 0 and d <= 0:
+                x += 1
+                delta += bPow2 + 2 * x * bPow2
+                draw_circle(x, origin_x, y, origin_y)
+                continue
+            x += 1
+            y -= 1
+            delta += bPow2 * (2 * x + 1) + aPow2 * (1 - 2 * y)
+            draw_circle(x, origin_x, y, origin_y)
+            i += 1
+            file.write(
+                f"Step {i}, e:{delta},d:{d},,d*{dz} x:{x + origin_x}, y:{y + origin_y}, Plot({x + origin_x},{y + origin_y})\n")
     file.close()
 
 
@@ -235,10 +300,12 @@ def handle_click(event):
     global start_point
     start_point = (event.x, event.y)
 
+
 def handle_release(event):
     global end_point
     end_point = (event.x, event.y)
     draw_line_based_on_algorithm(canvas)
+
 
 def draw_line_based_on_algorithm(canvas):
     if selected_algorithm == "DDA":
@@ -248,7 +315,10 @@ def draw_line_based_on_algorithm(canvas):
     elif selected_algorithm == "Wu":
         wu_algorithm(canvas)
     elif selected_algorithm == "circle":
-        circle(canvas)
+        circle_algorithm(canvas)
+    elif selected_algorithm == "ellipse":
+        ellipse_algorithm(canvas)
+
 
 def main():
     global canvas, file
@@ -259,7 +329,7 @@ def main():
     canvas = tk.Canvas(root, width=WIDTH, height=HEIGHT, bg=BG_COLOR)
     canvas.pack()
 
-    delite_button = tk.Button(root, text="Очистить", command=lambda: canvas.delete("all"))
+    delite_button = tk.Button(text="Очистить", command=lambda: canvas.delete("all"))
     delite_button.pack(side=tk.RIGHT)
 
     dda_button = tk.Button(root, text="ЦДА", command=lambda: select_algorithm("DDA"))
@@ -274,6 +344,9 @@ def main():
     circle_button = tk.Button(root, text="Окружность", command=lambda: select_algorithm("circle"))
     circle_button.pack(side=tk.LEFT)
 
+    ellipse_button = tk.Button(root, text="Эллипс", command=lambda: select_algorithm("ellipse"))
+    ellipse_button.pack(side=tk.LEFT)
+
     debug_button = tk.Button(root, text="Отладка", command=toggle_debug_mode)
     debug_button.pack(side=tk.LEFT)
 
@@ -281,6 +354,7 @@ def main():
     canvas.bind("<ButtonRelease-1>", handle_release)
 
     root.mainloop()
+
 
 if __name__ == "__main__":
     main()
